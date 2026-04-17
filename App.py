c618f9465111e06c04c7d031441b8e270ff86bb8
Suman K N <sumankn26@outlook.com>
2026-04-17 10:47:17 +0530

    Update App.py

--- App.py
+++ App.py
@@ -23,7 +23,7 @@ groq_api_key = st.secrets["GROQ_API_KEY"]
 
 # ------------------ UI ------------------
 st.set_page_config(page_title="Dynamic RAG with Groq", layout="wide")
-st.image("PragyanAI_Transperent.png")
+st.image("EVO_chatbot_logo.jpg")
 st.title("Dynamic RAG with Groq, FAISS, and Llama3")
 
 # ------------------ SESSION STATE ------------------
